10965eadec26e34a0755eed71c5d6fdfd8d93ba2
Gavin D'souza <gavin18d@gmail.com>
2019-07-10 23:25:56 +0530

    added(reports): general ledger #1 iteration

--- accounting/accounting/report/general_ledger/general_ledger.js
+++ accounting/accounting/report/general_ledger/general_ledger.js
@@ -4,6 +4,51 @@
 
 frappe.query_reports["General Ledger"] = {
 	"filters": [
-
+		{
+            "label": __("Fiscal Year"),
+            "fieldname": "fiscal_year",
+			"fieldtype": "Link",
+			"options": "Fiscal Year",
+			on_change: function(){
+				let fiscal_year = frappe.query_report.get_filter_value('fiscal_year');
+				frappe.db.get_value('Fiscal Year', fiscal_year, ['year_start_date', 'year_end_date']).then(
+					response => {
+						let values = response.message;
+						frappe.query_report.set_filter_value('from_date', values.year_start_date);
+						frappe.query_report.set_filter_value('to_date', values.year_end_date);
+					});
+			}
+        },
+		{
+			"fieldname": "from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"reqd": 1
+		},
+		{
+			"fieldname": "to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"reqd": 1
+		},
+		{
+			"fieldname": "transaction_type",
+			"label": __("Transaction Type"),
+			"fieldtype": "Select",
+			"options": [
+				"",
+				__("Purchase Invoice"),
+				__("Sales Invoice"),
+				__("Journal Entry")
+			]
+		},
+		{
+			"fieldname": "account",
+			"label": __("Account"),
+			"fieldtype": "Link",
+			"options": "Account"
+		}
 	]
 };
--- accounting/accounting/report/general_ledger/general_ledger.py
+++ accounting/accounting/report/general_ledger/general_ledger.py
@@ -2,8 +2,176 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+from frappe import _, _dict
+from frappe.utils import getdate, flt
+import frappe
+
 
 def execute(filters=None):
-	columns, data = [], []
-	return columns, data
+    if not filters:
+        return [], []
+
+    account_details = _dict()
+
+    for acc in frappe.db.sql("select name, is_group from tabAccount", as_dict=1):
+        account_details[acc.name] = acc
+
+    # validations
+    if filters.from_date > filters.to_date:
+        frappe.throw(_("From Date must be before To Date"))
+
+    columns = get_columns()
+    data = get_data(filters, account_details)
+
+    return columns, data
+
+
+def get_gl_entries(filters):
+    general_ledger_entries = frappe.db.sql(
+        "select * from `tabGL Entry` {} order by posting_datetime".format(
+            conditions(filters)),
+        filters,
+        as_dict=1
+    )
+
+    return general_ledger_entries
+
+
+def conditions(filters):
+    if filters.get("account"):
+        lft, rgt = frappe.db.get_value(
+            "Account", filters["account"], ["lft", "rgt"])
+        conditions = "account in (select name from tabAccount where lft >= {} and rgt <= {})".format(
+            lft, rgt)
+
+    else:
+        conditions = "posting_datetime >= %(from_date)s and posting_datetime <=%(to_date)s"
+
+    return "where {}".format(conditions)
+
+
+def get_data_with_opening_closing(filters, account_details, gl_entries):
+    gle_map = _dict()
+    totals, entries = get_accountwise_gle(filters, gl_entries, gle_map)
+    data = [totals.opening] + entries + [totals.total] + [totals.closing]
+
+    def _get_balance(row, balance, debit_field, credit_field):
+        balance += (row.get(debit_field, 0) - row.get(credit_field, 0))
+        return balance
+
+    for data_row in data:
+        data_row['against'] = data_row.get('against_account')
+
+        if not data_row.get('posting_datetime'):
+            balance = 0
+
+        balance = _get_balance(data_row, balance, 'debit', 'credit')
+        data_row['balance'] = balance
+
+    return data
+
+
+def get_accountwise_gle(filters, general_ledger_entries, gle_map):
+    from_date, to_date = getdate(filters.from_date), getdate(filters.to_date)
+    totals = get_totals()
+    entries = []
+
+    def _update_value_in_dict(data, key, gle):
+        data[key].debit += flt(gle.debit)
+        data[key].credit += flt(gle.credit)
+
+    for gle in general_ledger_entries:
+        if gle.posting_datetime.date() < from_date:
+            _update_value_in_dict(totals, 'opening', gle)
+            _update_value_in_dict(totals, 'closing', gle)
+
+        elif gle.posting_datetime.date() <= to_date:
+            _update_value_in_dict(totals, 'total', gle)
+            _update_value_in_dict(totals, 'closing', gle)
+            entries += [gle]
+
+    return totals, entries
+
+
+def get_totals():
+    def _get_debit_credit_dict(label):
+        return _dict(
+            account="'{0}'".format(label),
+            debit=0.0,
+            credit=0.0
+        )
+
+    return _dict(
+        opening=_get_debit_credit_dict(_('Opening')),
+        total=_get_debit_credit_dict(_('Total')),
+        closing=_get_debit_credit_dict(_('Closing (Opening + Total)'))
+    )
+
+
+def get_data(filters, account_details):
+    general_ledger_entries = get_gl_entries(filters)
+    data = get_data_with_opening_closing(
+        filters, account_details, general_ledger_entries)
+
+    return data
+
+
+def get_columns():
+    columns = [
+        {
+            "label": _("Reference Doc"),
+            "fieldname": "reference_doc",
+            "fieldtype": "Dynamic Link",
+            "width": 90
+        },
+        {
+            "label": _("Posting Timestamp"),
+            "fieldname": "posting_datetime",
+            "fieldtype": "Date",
+            "width": 90
+        },
+        {
+            "label": _("Account"),
+            "fieldname": "account",
+            "fieldtype": "Link",
+            "options": "Account",
+            "width": 180
+        },
+        {
+            "label": _("Debit (INR)"),
+            "fieldname": "debit",
+            "fieldtype": "Float",
+            "width": 100
+        },
+        {
+            "label": _("Credit (INR)"),
+            "fieldname": "credit",
+            "fieldtype": "Float",
+            "width": 100
+        },
+        {
+            "label": _("Balance (INR)"),
+            "fieldname": "balance",
+            "fieldtype": "Float",
+            "width": 130
+        },
+        {
+            "label": _("Transaction Type"),
+            "fieldname": "transaction_type",
+            "width": 120
+        },
+        {
+            "label": _("Against Account"),
+            "fieldname": "against_account",
+            "width": 120
+        },
+        {
+            "label": _("Party"),
+            "fieldname": "party",
+            "fieldtype": "Link",
+            "options": "Party",
+            "width": 100
+        }
+    ]
+
+    return columns
