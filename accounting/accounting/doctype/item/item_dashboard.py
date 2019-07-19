89d4af61a61cdf8817ecdc5015810df0235ca57c
Gavin D'souza <gavin18d@gmail.com>
2019-07-19 15:20:19 +0530

    added: dashboard for item, party

---
+++ accounting/accounting/doctype/item/item_dashboard.py
@@ -0,0 +1,14 @@
+def get_data():
+    return {
+        'fieldname': 'item',
+        'transactions': [
+            {
+                'label': 'Selling',
+                'items': ['Sales Invoice']
+            },
+            {
+                'label': 'Buying',
+                'items': ['Purchase Invoice']
+            }
+        ]
+    }
---
+++ accounting/accounting/doctype/party/party_dashboard.py
@@ -0,0 +1,14 @@
+def get_data():
+    return {
+        'fieldname': 'party',
+        'transactions': [
+            {
+                'label': 'Buying & Selling',
+                'items': ['Purchase Invoice', 'Sales Invoice']
+            },
+            {
+                'label': 'Payments',
+                'items': ['Payment Entry']
+            }
+        ]
+    }
